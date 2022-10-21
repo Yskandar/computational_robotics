0b87ca1679f170625085f67c109b03644c7fac4c
pquestel <philippequestel@hotmail.com>
2022-10-20 17:16:16 -0700

    Edges

--- Graph Based Motion Planning - Chess game/E_V.py
+++ Graph Based Motion Planning - Chess game/E_V.py
@@ -9,29 +9,48 @@ chess_grid = dict()
 count = 0
 for i in range(8):
     for j in range(8):
-        chess_grid[count] = [i,j]
+        chess_grid[count] = [j,i]
         count+=1
 
 print(chess_grid[0])
 
 V = chess_grid
 
-E = {}
-
-# Define a function that sets all the possible future states for current state and then we can search 
-# the value inside the dictionary and if it exists then there is an edge. 
+E = []
 
 
 for i in range(63):
-    if 0 <= V[i][0] + 2 <= 7:                          # x value
-        if 0 <= V[i][1] + 1 <= 7:                       # Plus 2 in x and + 1 in y
-            E[i] = [V[i],V[2*(i+1) + 1]]
-    else:
-        E[i] = 0
- 
+    if V[i][0] + 2 < 8:                 # +2 for x --> +/- 1 for y
+        if V[i][1] + 1 < 8:
+            next_state = i + 2 + 8
+            E.append([i,next_state])
+        if V[i][1] - 1 >= 0:
+            next_state = i + 2 - 8
+            E.append([i, next_state])
 
-print(V)
+    if V[i][0] - 2 >= 0:              # - 2 for x --> +/- 1 for y
+        if V[i][1] + 1 < 8:
+            next_state = i - 2 + 8
+            E.append([i, next_state])
+        if V[i][1] - 1 >= 0:
+            next_state = i - 2 - 8
+            E.append([i, next_state])
 
-
-# for i in range(63):
+    if V[i][1] + 2 < 8:                   # +2 in y --> +/- 1 in x
+        if V[i][0] + 1 < 8:
+            next_state = i + 16 + 1
+            E.append([i, next_state])
+        if V[i][0] - 1 >= 0:
+            next_state = i + 16 - 1
+            E.append([i, next_state])
     
+    if V[i][1] - 2 >= 0:                  # -2 in y --> +/- 1 in x
+        if V[i][0] + 1 < 8:
+            next_state = i - 16 + 1
+            E.append([i, next_state])
+        if V[i][0] - 1 >= 0:
+            next_state = i -16 - 1
+            E.append([i, next_state])   
+
+print(E)
+print(len(E))
