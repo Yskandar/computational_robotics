72045572a4f3e7ed2e56f5bb8bbeaeea16470376
Yskandar <33219452+Yskandar@users.noreply.github.com>
2022-10-27 17:31:29 -0700

    Implementing Depth First Search and comparing

--- Graph Based Motion Planning - Chess game/bfs_utils.py
+++ Graph Based Motion Planning - Chess game/bfs_utils.py
@@ -42,6 +42,7 @@ class Graph:
 
         return r
     
+    
     def get_neighbors(self, node):
         actions_array = np.array([[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]])
 
@@ -58,6 +59,9 @@ width = 8
 length = 8
 chess_board = Graph(width, length)
 
+
+
+'''
 for x in range(width):
     for y in range(length):
         chess_board.add_node([x,y])
@@ -68,7 +72,7 @@ for node in chess_board.nodes:
     edges = chess_board.get_edges(node)
     chess_board.add_edges(edges)
 
-
+'''
 
 def naive_breadth_first_search(graph, init_node):
 
@@ -103,7 +107,6 @@ def breadth_first_search(graph, init_node, goal_node):
             else:
                 if neighbor not in path and path + [neighbor] not in path_list:
                     path_list.append(path + [neighbor])
-                    print(path_list)
         k += 1
     
     print("no path detected")
@@ -115,17 +118,91 @@ def breadth_first_search(graph, init_node, goal_node):
 
 # Create the graph and all the nodes
 
-width = 3
-length = 3
+width = 8
+length = 8
 chess_board = Graph(width, length)
 
 
 
 init_node = [0, 0]
-goal_node = [10, 0]
-paths = breadth_first_search(chess_board, init_node, goal_node)
+goal_node = [6, 0]
+path_bfs = breadth_first_search(chess_board, init_node, goal_node)
 
 
+width = 4
+length = 4
+chess_board = Graph(width, length)
+
+# Let us now implement depth first search
+
+
+
+def naive_depth_first_search(chess_board, init_node):  # just explores the nodes
+    node_list = []
+    queue = [init_node]
+
+    while queue:
+        node = queue.pop(0)
+        node_list.append(node)
+        neighbors = chess_board.get_neighbors(node)
+        neighbors = [neighbor for neighbor in neighbors if neighbor not in queue + node_list]
+        if neighbors != []:
+            queue =  neighbors + queue
+    
+    return node_list
+
+
+# Let's try it !
+init_node = [0, 0]
+node_list = naive_depth_first_search(chess_board, init_node)
+print("The correct number of nodes is retrieved:", len(node_list) == len(chess_board.nodes))
+
+
+def depth_first_search(chess_board, init_node, goal_node):  # just explores the nodes
+    path_list = []
+    path_queue = [[init_node]]
+    while path_queue:
+        path = path_queue.pop(0)
+        #print(path)
+        node = path[-1]
+        
+        neighbors = chess_board.get_neighbors(node)
+        #print('neighbors:', neighbors)
+        for neighbor in neighbors:
+            if neighbor == goal_node:
+                return path + [neighbor]
+            else:
+                #print('neighbor', neighbor)
+                #print('path + neighbor', path + [neighbor])
+                if neighbor not in path and path + [neighbor] not in path_list:
+                    new_path = path + [neighbor]
+                    #print('new_path', new_path)
+                    path_queue = [new_path] + path_queue
+                    #print('path_queue', path_queue)
+                    path_list.append(new_path)
+
+    print('No path found')
+    return path_list
+
+
+
+
+# Create the graph and all the nodes
+
+width = 8
+length = 8
+chess_board = Graph(width, length)
+
+
+init_node = [0, 0]
+goal_node = [6, 0]
+path_dfs = depth_first_search(chess_board, init_node, goal_node)
+
+print('Length of the path found with breadth_first_search:', len(path_bfs))
+print('Length of the path found with depth_first_search:', len(path_dfs))
+
+
+# We can see that DFS definitely does not give you the shortest path...
 
 
 
